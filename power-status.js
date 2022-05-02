fcb1c7886481a1827ff955aba2546897526d1114
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2022-05-02 13:17:16 -0500

    finish starter code

--- power-status.js
+++ power-status.js
@@ -1,6 +1,6 @@
 
 
-if(ride1PowerOn === true){
+if (ride1PowerOn === true) {
     ride1PowerElement.style.color = "green"
     ride1PowerElement.textContent = "Power Status: on"
 }
@@ -9,7 +9,7 @@ else {
     ride1PowerElement.textContent = "Power Status: off"
 }
 
-if(ride2PowerOn === true){
+if (ride2PowerOn === true) {
     ride2PowerElement.style.color = "green"
     ride2PowerElement.textContent = "Power Status: on"
 }
@@ -18,7 +18,7 @@ else {
     ride2PowerElement.textContent = "Power Status: off"
 }
 
-if(ride3PowerOn === true){
+if (ride3PowerOn === true) {
     ride3PowerElement.style.color = "green"
     ride3PowerElement.textContent = "Power Status: on"
 }
@@ -27,7 +27,16 @@ else {
     ride3PowerElement.textContent = "Power Status: off"
 }
 
-if(ride4PowerOn === true){
+//power is on and all slots work right
+if (ride4PowerOn === true &&
+    ride4SlotMachine(1) == "star" &&
+    ride4SlotMachine(2) == "car" &&
+    ride4SlotMachine(3) == "pot of gold" &&
+    ride4SlotMachine(4) == "clover" &&
+    ride4SlotMachine(5) == "bowling ball" &&
+    ride4SlotMachine(6) == "tiger" &&
+    ride4SlotMachine(7) == "seven"
+) {
     ride4PowerElement.style.color = "green"
     ride4PowerElement.textContent = "Power Status: on"
 }
@@ -35,16 +44,28 @@ else {
     ride4PowerElement.style.color = "red"
     ride4PowerElement.textContent = "Power Status: off"
 }
-//if all match up you win
-let slot1 = ride4SlotMachine()
-let slot2 = ride4SlotMachine()
-let slot3 = ride4SlotMachine()
 
-let didTheyWin = (slot1 === slot2) && (slot2 === slot3)
 
-if (ride4PowerOn && didTheyWin){
-    setTimeout(()=> {
-        window.alert("You won the slot machine")
-    }, 500)
-    
-}
+let spinButton = document.querySelector("#spin")
+let slot1Div = document.querySelector("#slot-1")
+let slot2Div = document.querySelector("#slot-2")
+let slot3Div = document.querySelector("#slot-3")
+
+spinButton.addEventListener("click", function () {
+    //if all match up you win
+    let slot1 = ride4SlotMachine(3)
+    slot1Div.textContent = slot1
+    let slot2 = ride4SlotMachine(3)
+    slot2Div.textContent = slot1
+    let slot3 = ride4SlotMachine(3)
+    slot3Div.textContent = slot1
+    let didTheyWin = (slot1 === slot2) && (slot2 === slot3)
+
+    if (ride4PowerOn && didTheyWin) {
+        setTimeout(() => {
+            window.alert("You won the slot machine")
+        }, 500)
+
+    }
+})
+

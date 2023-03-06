559dfd6f7a031965738909b7266ba6e2e97a55e1
zachary-mathias@hotmail.com <zachary-mathias@hotmail.com>
2023-03-05 21:57:23 -0500

    still have to ask instructor why ride 4 is not working. Everything else appears to be correct.

--- index.js
+++ index.js
@@ -11,21 +11,26 @@ let waterIsOn = true
 
 //Ride 1 works when:
     // green wire is connected
-ride1PowerOn = false
-
+if(greenWireConnected){
+    ride1PowerOn = true
+}
 //Ride 2 works when 
     //green and yellow wires are connected
-ride2PowerOn = false
-
+if(greenWireConnected && yellowWireConnected){
+    ride2PowerOn = true
+}
 //Ride 3 works when:
     // green and red wires are connected
     // water is on 
     // yellow wire is not connected
-ride3PowerOn = false
-
+if(greenWireConnected && redWireConnected && waterIsOn){
+    ride3PowerOn = true
+    }
 //Ride 4 has power when:
     //ride wire is connected
-ride4PowerOn = false
+if(redWireConnected){
+    ride4PowerOn = true
+}
 //Ride 4 is a slot machine
     //if a 1 is rolled it should return "star"
     //if a 2 is rolled it should return "car"
